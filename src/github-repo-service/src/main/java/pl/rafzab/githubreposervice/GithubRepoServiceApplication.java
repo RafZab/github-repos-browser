ec795a6e9f96253fce3de68e4b9b287fe3fc2e26
Rafal Zaborowski <raffik2000@gmail.com>
2025-07-16 17:44:23 +0200

    init github repo service

---
+++ src/github-repo-service/src/main/java/pl/rafzab/githubreposervice/GithubRepoServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.rafzab.githubreposervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepoServiceApplication.class, args);
+    }
+
+}
---
+++ src/github-repo-service/src/test/java/pl/rafzab/githubreposervice/GithubRepoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rafzab.githubreposervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
